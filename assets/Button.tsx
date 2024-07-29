1836b8c7665c8f404f9539aa4bd3d578193fa43d
yashladekar <yashladekar002@gmail.com>
2024-07-30 03:20:05 +0530

    chore: Add initial project configuration files and dependencies along with basic bundling with webpack

---
+++ assets/Button.tsx
@@ -0,0 +1,6 @@
+import * as react from "react";
+interface ButtonProps {
+  label: string;
+  onClick: () => void;
+  type: string;
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import React from "react";
+
+function index() {
+  return React.createElement("div", null, "index");
+}
+
+export default index;
